3ef929bd0d8eaf772f31369311373f81a5ad2444
AnikaAishwarja <Anika.I.Aishwarja@student.uts.edu.au>
2022-07-14 22:02:50 +1000

    Create 158A Next Round.py

---
+++ 1A Theatre Square.py
@@ -0,0 +1,23 @@
+import math
+lst = list(map(int, input().split()))
+n = lst[0] #4
+m = lst[1] #6
+a = lst[2] #4
+# print(round(m/a))
+
+f = 0
+if a>n and a>m:
+    print(1)
+elif a % n != 0 and a % m != 0:
+    f = math.ceil(n/a) * math.ceil(m / a)
+    print(f)
+elif a % n != 0 and a % m == 0:
+    f = math.ceil(n / a)
+    print(f)
+elif a % n == 0 and a % m != 0:
+    f = (math.ceil(n/a) + math.ceil(m / a))-1
+    print(f)
+else:
+    print(1)
+
+
