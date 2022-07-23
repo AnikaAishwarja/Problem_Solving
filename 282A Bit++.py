33679a74b008aa338f082050682700d7b037065d
AnikaAishwarja <Anika.I.Aishwarja@student.uts.edu.au>
2022-07-23 12:29:32 +1000

    Update 282A Bit++.py

--- 282A Bit++.py
+++ 282A Bit++.py
@@ -1,7 +1,11 @@
 num_of_statements = int(input())
 lst = []
+count = 0
+for i in range(0, num_of_statements):
+    ele = str(input())
+    if ele == "++X" or ele == "X++":
+        count = count + 1
+    else:
+        count = count - 1
 
-for i in range(0, num_of_statements+1):
-    lst[i] = str(input())
-
-print(lst)
+print(count)
